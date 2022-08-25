d76254e52a87ad7a647e6caba22e120e51926db8
VianaDias <109551825+MatheusVianaDias@users.noreply.github.com>
2022-08-25 02:52:12 -0300

    att-projeto-PingPong

--- ping.js
+++ ping.js
@@ -16,35 +16,52 @@ let yRaquete = 190;
 
 function setup() {
     createCanvas(800, 500);
-  }
-  
-  function draw() {
+}
+
+function draw() {
     background(0);
     mostraBolinha();
     movimentaBolinha();
     verificaBatidaBorda();
     mostraRaquete();
+    movimentaMinhaRaquete();
+    verificaColosaoRaquete();
 
 }
 
-function mostraBolinha(){
-    circle(xBolinha, yBolinha,diametro);
+function mostraBolinha() {
+    circle(xBolinha, yBolinha, diametro);
 }
 
-function movimentaBolinha(){
+function movimentaBolinha() {
     xBolinha += velocidadeXBolinha;
     yBolinha += velocidadeYBolinha;
 }
 
-function verificaBatidaBorda(){
-    if (xBolinha + raio > width || xBolinha - raio < 0 ){
+function verificaBatidaBorda() {
+    if (xBolinha + raio > width || xBolinha - raio < 0) {
         velocidadeXBolinha *= -1;
     }
-    if (yBolinha + raio > height || yBolinha - raio < 0){
+    if (yBolinha + raio > height || yBolinha - raio < 0) {
         velocidadeYBolinha *= -1;
-    }   
+    }
 }
 
-function mostraRaquete(){
+function mostraRaquete() {
     rect(xRaquete, yRaquete, raqueteComprimento, raqueteAltura);
+}
+
+function movimentaMinhaRaquete() {
+    if (keyIsDown(UP_ARROW)) {
+        yRaquete -= 10;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        yRaquete += 10;
+    }
+}
+
+function verificaColosaoRaquete(){
+    if (xBolinha - raio < xRaquete + raqueteComprimento && yBolinha - raio < yRaquete + raqueteAltura && yBolinha + raio > yRaquete ){
+        velocidadeXBolinha *= -1;
+    }
 }
